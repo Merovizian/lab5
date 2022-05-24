476cec1815238ea02c691cf93c65d75de55e02d4
Merovizian <ericgmicaela@gmail.com>
2022-05-24 08:15:36 -0300

    Versão 1

--- servidor-tcp.py
+++ servidor-tcp.py
@@ -5,13 +5,12 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
-print(PORT,HOST)
 while True:
     con, cliente = tcp.accept()
     print 'Concetado por', cliente
     while True:
         msg = con.recv(1024)
         if not msg: break
-            print(cliente, msg)
+            print(msg)
             print('Finalizando conexao do cliente', cliente)
     con.close()
