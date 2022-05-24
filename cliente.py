1f6bdbc57ad22b2517546a7406f0267958dc0cbb
Merovizian <ericgmicaela@gmail.com>
2022-05-24 08:05:44 -0300

    Versão 2

--- cliente.py
+++ cliente.py
@@ -4,11 +4,9 @@ PORT = 5000 # Porta que o Servidor esta
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dest = (HOST, PORT)
 print('Para sair use CTRL+X\n')
-msg2 = raw_input("Digite sua mensagem: ")
+msg = raw_input("Digite sua mensagem: ")
 
-
-print(type(msg2))
-while msg2 <> '\x18':
-    udp.sendto(msg2, dest)
-    msg2 = raw_input("Digite sua mensagem: ")
+while msg <> '\x18':
+    udp.sendto(msg, dest)
+    msg = raw_input("Digite sua mensagem: ")
     udp.close()
